cf640a5b31522f941e2fed7859620d39c021c9f9
mukeyshh <mrkhadka17@gmail.com>
2021-03-12 11:05:05 +0545

    first commit

---
+++ svelte-scoreboard/src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+
+	import Navbar from "./Navbar.svelte";
+	import Player from "./Player.svelte";
+	import AddPlayer from "./AddPlayer.svelte";
+
+	let players=[
+		{
+			name:"mukesh",
+			points:50
+		},
+		{
+			name:"sandip",
+			points:79
+
+		},
+	];
+
+	// react ma jastai
+
+	const addPlayer=(e)=>{
+		const newPlayer=e.detail;
+		players = [...players, newPlayer]
+	}
+
+	const removePlayer=(e)=>{
+		players=players.filter(player=>player.name!== e.detail);
+	}
+
+</script>
+
+<Navbar />
+<div class="contaner">
+	<AddPlayer on:addplayer={addPlayer} />
+	{#if players.length===0}
+	<p>no players</p>
+	{:else}
+	{#each players as player}
+	<Player
+	name={player.name}
+	points={player.points}
+	on:removeplayer={removePlayer}
+	/>
+	{/each}
+	{/if}
+</div>
---
+++ svelte-scoreboard/src/Player.svelte
@@ -0,0 +1,47 @@
+<script>
+
+import {createEventDispatcher} from 'svelte';
+
+ const dispatch = createEventDispatcher();
+
+	export let name;
+	export let points;
+	let showControls=false;
+
+	const addPoint=()=>(points+=1);
+	const removePoint=()=>(points-=1);
+	const toggle=()=>(showControls=!showControls)
+    const onDelete=()=>dispatch("removeplayer",name);
+
+</script>
+
+
+
+<style>
+	h1{
+		color: blue;
+	}
+	h3{
+		margin-bottom:10px ;
+	}
+</style>
+
+
+
+
+	<div class="card">
+	<h1>
+		Hello {name}
+		<button class="btn" on:click={toggle}>{#if showControls}-{:else}+{/if}</button>
+        <button class="btn btn-danger btn-sm" on:click={onDelete}>Remove player</button>
+	</h1>
+	<h3>Points:{points}</h3>
+{#if showControls}
+
+	<button class="btn" on:click={addPoint}>+1</button>
+	<button class="btn btn-dark" on:click={removePoint}>-1</button>
+	<input type="number" bind:value={points}>
+
+{/if}
+</div>
+
